df7efd1a1c7538f179460d23a5532f27a35157c7
Morten Piibeleht <morten.piibeleht@gmail.com>
2016-04-13 19:08:26 +0200

    Add the Asymmetric type
    An asymmetric matrix is missing in Julia Base.

--- src/juliarc.jl
+++ src/juliarc.jl
@@ -62,4 +62,41 @@ function DataFrame(ps::Pair...)
     DF
 end
 
+
+# An ansymmetric matrix
+import Base: length, size, getindex
+
+"""
+    Asymmetric{T} <: AbstractArray{T,2}
+
+Defines a MxM asymmetric matrix. Data is stored in a
+vector.
+"""
+type Asymmetric{T} <: AbstractArray{T,2}
+    M::Int
+    data::Vector{T}
+end
+
+length(mat::Asymmetric) = mat.M*(mat.M-1)
+size(mat::Asymmetric) = (mat.M, mat.M)
+
+getindex(mat::Asymmetric, i) = mat.data[i]
+function getindex{T}(mat::Asymmetric{T}, m,n)
+    M = mat.M
+    if !(1 <= m <= M && 1 <= n <= M)
+        error("Index ($m,$n) out of bounds.")
+    end
+    if m == n
+        return zero(T)
+    elseif m < n
+        i = div((n-2)*(n-1), 2)+m
+        mat[i]
+    else
+        i = div((m-2)*(m-1), 2)+n
+        -mat[i]
+    end
+    
+end
+
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,6 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+import juliarc: Asymmetric
+@test sum(Asymmetric(3, [1,2,3]) .== [0 1 2; -1 0 3; -2 -3 0]) == 9
