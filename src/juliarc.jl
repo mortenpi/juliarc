c212fefc7816556d3ae6800449374dddd16f09de
Morten Piibeleht <morten.piibeleht@gmail.com>
2016-11-01 19:23:44 +0100

    Update dropping streams

--- src/juliarc.jl
+++ src/juliarc.jl
@@ -1,3 +1,21 @@
+"""
+# Dropping streams
+
+E.g. if there's too much error / deprecation output that can safely be ignored.
+
+```julia
+@drop_streams juliarc.load(:gadfly)
+```
+
+Especially relevant for clusters where the amount of output is multiplied by the
+number of processes:
+
+```julia
+import juliarc
+@everywhere using juliarc
+@everywhere @drop_streams juliarc.load(:gadfly)
+```
+"""
 module juliarc
 
 import Compat
@@ -122,52 +140,100 @@ end
 wontreturn(::Any) = true
 
 
-# Redirect STDERR
-export @drop_stderr
+# Redirect STDOUT and STDERR streams
+export @drop_streams
 
-"""Stores the `STDERR` output redirected by `@drop_stderr`."""
-const dropped_stderr_ref = Ref{Compat.UTF8String}("")
+"""Stores the dropped `STDOUT` and `STDERR` output redirected by `@drop_streams`."""
+const stored_streams = Ref{Tuple{Compat.UTF8String,Compat.UTF8String}}(("",""))
 
 """
-    dropped_stderr()
-    dropped_stderr(x)
+    dropped_streams()
+    dropped_streams(out, err)
 
-Returns or sets the `dropped_stderr_ref` variable.
+Returns or sets the `stored_streams` variable.
 
 To pretty print its contents:
-    juliarc.dropped_stderr() |> println
+```julia
+    # for STDOUT
+    juliarc.dropped_streams() |> first |> println
+    # for STDERR
+    juliarc.dropped_streams() |> last |> println
+```
 """
-dropped_stderr() = dropped_stderr_ref.x
-dropped_stderr(x) = dropped_stderr_ref.x = x
+dropped_streams() = stored_streams.x
+dropped_streams(out, err) = stored_streams.x = (out, err)
 
 """
-    @drop_stderr ex
+    @drop_streams ex
 
 Redirects the STDERR and stores it in a global variable (accessible via the
-`dropped_stderr` functions).
+`dropdropped_streamsped_stderr` functions).
 
 # Usage
 
-```
-@drop_stderr warn(".")
-@drop_stderr begin
+```julia
+@drop_streams warn("...")
+@drop_streams begin
     using Gadfly
     warn("More STDERR")
 end
 ```
 """
-macro drop_stderr(ex)
+macro drop_streams(ex)
     ex = wontreturn(ex) ? :($(esc(ex)); ret=nothing) : :(ret = $(esc(ex)))
     quote
-        _STDERR = STDERR
-        r,w = redirect_stderr()
+        _STDOUT, _STDERR = STDOUT, STDERR
+        out_r, out_w = redirect_stdout()
+        err_r, err_w = redirect_stderr()
         $ex
+        redirect_stdout(_STDOUT)
         redirect_stderr(_STDERR)
-        close(w)
-        readavailable(r) |> Compat.UTF8String |> dropped_stderr
-        close(r)
+        close(out_w)
+        close(err_w)
+        _out = readavailable(out_r) |> Compat.UTF8String
+        _err = readavailable(err_r) |> Compat.UTF8String
+        dropped_streams(_out, _err)
+        close(out_r)
+        close(err_r)
         ret
     end
+end
+
+# An alternative implementation without macros
+type StreamRedirects
+    STDOUT
+    out_r :: Base.PipeEndpoint
+    out_w :: Base.PipeEndpoint
+    out_buf :: Compat.UTF8String
+    STDERR
+    err_r :: Base.PipeEndpoint
+    err_w :: Base.PipeEndpoint
+    err_buf :: Compat.UTF8String
+    StreamRedirects() = new()
+end
+
+const redirector = StreamRedirects()
+
+function redirect_streams()
+    r = redirector
+    r.STDOUT = STDOUT
+    r.out_r, r.out_w = redirect_stdout()
+    r.STDERR = STDERR
+    r.err_r, r.err_w = redirect_stderr()
+    return nothing
+end
+
+function restore_streams()
+    r = redirector
+    redirect_stderr(r.STDERR)
+    redirect_stdout(r.STDOUT)
+    close(r.err_w)
+    close(r.out_w)
+    r.err_buf = readavailable(r.err_r) |> Compat.UTF8String
+    r.out_buf = readavailable(r.out_r) |> Compat.UTF8String
+    close(r.err_r)
+    close(r.out_r)
+    return nothing
 end
 
 end # module
