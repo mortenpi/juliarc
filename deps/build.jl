5129c08eda3dcaef4651c4d6478c6744c6b5440e
Morten Piibeleht <morten.piibeleht@gmail.com>
2017-02-07 18:42:29 +1300

    Check whether Orthopolys is installed
    Otherwise it failed with an error if it was already installed.

--- deps/build.jl
+++ deps/build.jl
@@ -1,1 +1,6 @@
-Pkg.clone("https://github.com/mortenpi/Orthopolys.jl.git")
+# The Orthopolys packages is not in the package repo
+orthopolys_installed = false
+try
+    orthopolys_installed = (Pkg.installed("Orthopolys") !== nothing)
+catch end
+orthopolys_installed || Pkg.clone("https://github.com/mortenpi/Orthopolys.jl.git")
